29e629839eaacedbe86611232eee3e5b71022017
Kamil Mustafayev <github-email-kamil.mstfyv@gmail.com>
2026-06-05 17:54:28 +0400

    feat: backend crud operations and rest client added

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello express</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    // Dümdüz bitirmək əvəzinə mesaj göndərmək:
+    response
+      .status(404)
+      .send({ error: "Axtardığınız qeyd verilənlər bazasında tapılmadı!" });
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const generateid = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => +n.id)) : 0;
+  return String(maxId + 1);
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).send({
+      error: "content missed",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateid(),
+  };
+  notes = notes.concat(note);
+  response.json(note);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`started at ${PORT}`);
+});
+console.log([1, 2, 3]);
